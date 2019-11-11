d11fe1a3a2bd1470c1151381d9b08f33cf8b0bef
guilhermegiarola <gheg@estudante.ufla.br>
2019-11-11 18:24:28 -0300

    Add files via upload

--- kNearestNeighbors.py
+++ kNearestNeighbors.py
@@ -1,4 +1,5 @@
 from generateList import generateList
+import matplotlib.pyplot as plt
 import math
 import itertools
 import os 
@@ -8,15 +9,18 @@ class kNearestNeighbors:
     handInput = []
     orderedList = {}
     comparedNeighbors = []
-    kNeighbors = 0
+    kNeighbors = []
+    accuracyList = []
 
     def __init__(self, fileName):
         auxList = generateList()
         auxList.readArchive(fileName)
         
-        print("Insert the number of neighbors to be compared: ")
-        self.kNeighbors = input()
-        
+        aux = 3
+        while(aux < 75):
+            self.kNeighbors.append(aux)
+            aux = aux + 2
+
     def getDistance(self, firstHand, secondHand):
         summation = 0
         for i in range(0,len(firstHand)-1):
@@ -25,7 +29,7 @@ class kNearestNeighbors:
             summation = float(summation) + float(value1 - value2)**2
         return math.sqrt(summation)
     
-    def executeKNN(self, entry):
+    def executeKNN(self, entry, num):
         auxList = generateList()
         auxList.readArchive('iris.csv')
         auxValue = []
@@ -40,7 +44,7 @@ class kNearestNeighbors:
         auxIterator = 0
 
         for distance, value in sorted(self.orderedList.items()):
-            if(auxIterator < int(self.kNeighbors)):
+            if(auxIterator < int(num)):
                 self.comparedNeighbors.append(value[4])
                 auxIterator += 1
 
@@ -81,38 +85,59 @@ class kNearestNeighbors:
                 confusionMatrix[i].append(0)
 
         auxList = generateList()
-        auxList.readArchive('iris.csv')
+        auxList.readArchive('test.csv')
 
-        for i in range(0, 150):
-            entry = auxList.readLine(i)
-            result = self.executeKNN(auxList.readLine(i))
+        for j in self.kNeighbors:
+            for i in range(0, 75):
+                entry = auxList.readLine(i)
+                result = self.executeKNN(auxList.readLine(i), j)
 
-            if(entry[4] == 'Iris-setosa' and result == 'Iris-setosa'):
-                confusionMatrix[0][0] += 1
-            elif(entry[4] == 'Iris-setosa' and result == 'Iris-versicolor'):
-                confusionMatrix[0][1] += 1
-            elif(entry[4] == 'Iris-setosa' and result == 'Iris-virginica'):
-                confusionMatrix[0][2] += 1
-            if(entry[4] == 'Iris-versicolor' and result == 'Iris-setosa'):
-                confusionMatrix[1][0] += 1
-            elif(entry[4] == 'Iris-versicolor' and result == 'Iris-versicolor'):
-                confusionMatrix[1][1] += 1
-            elif(entry[4] == 'Iris-versicolor' and result == 'Iris-virginica'):
-                confusionMatrix[1][2] += 1
-            elif(entry[4] == 'Iris-virginica' and result == 'Iris-setosa'):
-                confusionMatrix[2][0] += 1
-            elif(entry[4] == 'Iris-virginica' and result == 'Iris-versicolor'):
-                confusionMatrix[2][1] += 1
-            elif(entry[4] == 'Iris-virginica' and result == 'Iris-virginica'):
-                confusionMatrix[2][2] += 1
+                if(entry[4] == 'Iris-setosa' and result == 'Iris-setosa'):
+                    confusionMatrix[0][0] += 1
+                elif(entry[4] == 'Iris-setosa' and result == 'Iris-versicolor'):
+                    confusionMatrix[0][1] += 1
+                elif(entry[4] == 'Iris-setosa' and result == 'Iris-virginica'):
+                    confusionMatrix[0][2] += 1
+                if(entry[4] == 'Iris-versicolor' and result == 'Iris-setosa'):
+                    confusionMatrix[1][0] += 1
+                elif(entry[4] == 'Iris-versicolor' and result == 'Iris-versicolor'):
+                    confusionMatrix[1][1] += 1
+                elif(entry[4] == 'Iris-versicolor' and result == 'Iris-virginica'):
+                    confusionMatrix[1][2] += 1
+                elif(entry[4] == 'Iris-virginica' and result == 'Iris-setosa'):
+                    confusionMatrix[2][0] += 1
+                elif(entry[4] == 'Iris-virginica' and result == 'Iris-versicolor'):
+                    confusionMatrix[2][1] += 1
+                elif(entry[4] == 'Iris-virginica' and result == 'Iris-virginica'):
+                    confusionMatrix[2][2] += 1
+        
+                accuracy = 0
+                for i in range(0, len(confusionMatrix[0])):
+                    accuracy = accuracy + confusionMatrix[i][i]
+                
+            for i in range(0,3):
+                for j in range(0,3):
+                    confusionMatrix[i][j] = 0
 
-        for i in range(0, len(confusionMatrix[0])):
-            print(confusionMatrix[i])    
+            accuracy = accuracy*100/75
+            self.accuracyList.append(accuracy)
+            accuracy = 0
+    
+    def plotarGrafico(self):
+        fig = plt.figure()
 
-#---------------------------------Main Code-----------------------------------------        
+        ax1 = fig.add_subplot(111)
+        ax1.set_title("Accuracy x Quantity of Neighbors(k)")
+        ax1.set_ylabel('Accuracy')
+        ax1.set_xlabel('Quantity of Neighbors')
+
+        plt.plot(self.kNeighbors, self.accuracyList)
+        plt.show()
+#---------------------------------Main Code-----------------------------------------#        
 classList = generateList()
 classList.readArchive('iris.csv')
+
 execution = kNearestNeighbors('iris.csv')
 
 execution.confusionMatrix()
-    
+execution.plotarGrafico()
